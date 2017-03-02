2fa8a77eb3bf6a844ba533a5bf1767487fce3841
JoseCedeno21 <jose.gabriell.21@gmail.com>
2017-03-02 00:23:30 -0500

    validacion de correo/ validacion de estudiantes al crear curso

--- Sandbox/public/js/sandbox/cursos/agregarEstudiantes.js
+++ Sandbox/public/js/sandbox/cursos/agregarEstudiantes.js
@@ -1,11 +1,40 @@
 $(document).ready(function() {
 	$('#agregarE').click(function(){
 		var estudiante = $("#estudiantes").val();
-		console.log(estudiante);
-		if (estudiante != ""){
-			$('#lista-group').append('<div class="row"><div class="col-sm-4 col-md-4" id="estd">'+estudiante+'</div><button class="btn btn-danger quitar-entrada" data-target="div' + estudiante + '">Quitar</button><input type="hidden" name="est" value="'+estudiante+'"></div>')
-		}
+		//console.log(estudiante);
+		var cont = 0;
+		$.ajax({
+            url: "/cursos/crear/obtener",
+            dataType: "json",
+            success: function (data) {
+              
+                $.map(data, function(v,i){
+                  var est = v.nombres + " " + v.apellidos;
+
+                  if (est == estudiante){
+                    console.log("entra");
+                 	cont = 1;
+
+                  }
+                });
+                
+		        if (cont == 0){
+		        	$("#msg-estudiantes").removeClass("hidden");
+		            $("#estudiantes-group").addClass("has-error");
+		        } else if (estudiante != ""){
+		        	$("#msg-estudiantes").addClass("hidden");
+		            $("#estudiantes-group").removeClass("has-error");
+		        	$('#lista-group').append('<div class="row"><div class="col-sm-4 col-md-4" id="estd">'+estudiante+'</div><button class="btn btn-danger quitar-entrada" data-target="div' + estudiante + '">Quitar</button><input type="hidden" name="est" value="'+estudiante+'"></div>');
+		        	$("#estudiantes").val("");
+
+		        }
+            }
+        });
+
+        
+
 	});
+
 	$('body').on('click', '.quitar-entrada', function() {
         $(this).parent().remove();
         $()
---
+++ Sandbox/public/js/sandbox/usuarios/usuarios_editar.js
@@ -0,0 +1,77 @@
+$('#editarUsuario').click(function (event) {
+        //event.preventDefault();
+        var nombres = $("#nombres").val();
+        var apellidos = $("#apellidos").val();
+        var correo = $("#correo").val();
+        var identificacion = $("#identificacion").val();
+        var carrera = $("#carrera").val();
+        var pass = $("#pass").val();
+        var camposLlenos = 0;
+        var campoNombres, campoApellidos, campoCorreos, campoIdentificacion, campoCarrera, campoPass;
+
+        if (nombres === "") {
+            $("#msg-nombres").removeClass("hidden");
+            $("#nombres-group").addClass("has-error");
+            campoNombres = 0;
+        } else {
+            $("#msg-nombres").addClass("hidden");
+            $("#nombres-group").removeClass("has-error");
+            campoNombres = 1;
+        }
+
+        if (apellidos === "") {
+            $("#msg-apellidos").removeClass("hidden");
+            $("#apellidos-group").addClass("has-error");
+            campoApellidos = 0;
+        } else {
+            $("#msg-apellidos").addClass("hidden");
+            $("#apellidos-group").removeClass("has-error");
+            campoApellidos = 1;
+        }
+
+        if (correo === "") {
+            $("#msg-correo").removeClass("hidden");
+            $("#correo-group").addClass("has-error");
+            campoCorreo = 0;
+        } else {
+            $("#msg-correo").addClass("hidden");
+            $("#correo-group").removeClass("has-error");
+            campoCorreo = 1;
+        }
+
+        if (identificacion === "") {
+            $("#msg-identificacion").removeClass("hidden");
+            $("#identificacion-group").addClass("has-error");
+            campoIdentificacion = 0;
+        } else {
+            $("#msg-identificacion").addClass("hidden");
+            $("#identificacion-group").removeClass("has-error");
+            campoIdentificacion = 1;
+        }
+
+        if (carrera === "") {
+            $("#msg-carrera").removeClass("hidden");
+            $("#carrera-group").addClass("has-error");
+            campoCarrera = 0;
+        } else {
+            $("#msg-carrera").addClass("hidden");
+            $("#carrera-group").removeClass("has-error");
+            campoCarrera = 1;
+        }
+
+        if (pass === "") {
+            $("#msg-pass").removeClass("hidden");
+            $("#pass-group").addClass("has-error");
+            campoPass = 0;
+        } else {
+            $("#msg-pass").addClass("hidden");
+            $("#pass-group").removeClass("has-error");
+            campoPass = 1;
+        }
+
+        camposLlenos = campoNombres + campoApellidos + campoCorreo + campoIdentificacion + campoCarrera + campoPass;
+        
+        if (camposLlenos === 6) {
+            $("#editarUsuario").attr("data-target","#myModal");
+        }
+});
